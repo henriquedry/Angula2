183315f98cab917b89b74be4e0efd5f299fe647e
Pedro Henrique <phdr131@gmail.com>
2018-03-26 21:32:57 -0300

    aula 26/03

---
+++ src/app/domain/mock-heroes.ts
@@ -0,0 +1,11 @@
+import {Hero} from "./hero";
+
+export const HEROES : Hero[] = [
+    {id:1, name: 'Hulk'},
+    {id:2, name: 'Doutor Estranho'},
+    {id:3, name: 'Homem de Ferro'},
+    {id:4, name: 'Arqueiro Verde'},
+    {id:5, name: 'Kid Flash'},
+    {id:6, name: 'Tornado Vermelho'},
+    {id:7, name: 'Visão'},
+]
